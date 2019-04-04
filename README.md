25d502df4052a898f7c3d1126bbc1fcbbb19515a
Kino  Rose <kinoarose@gmail.com>
2019-04-03 21:39:32 -0400

    finish initial version for converting all files to jpeg

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image Compressor
+> Author: Kino Rose
+> A simple image compression solution
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-/* Allows you to compress an image to a lower quality file size */
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
 
+/* 
+* Allows you to compress an image to a lower quality file size within the src/ folder 
+* Author: Kino Rose
+*/
+
+const outDir = path.resolve("./out")
+fs.readdirSync(path.resolve("src"))
+.filter( filename =>  /\.jpeg|\.jpg|\.png|\.svg/g.test(filename))
+.forEach( filename => {
+  console.log(filename)
+  sharp(path.join("./src", filename))
+  .resize(860, 640)
+  .jpeg({
+    quality:80
+  })
+  .toFile(`${outDir}/${filename}-reduced.jpeg`)
+  .catch(console.error);
+})
